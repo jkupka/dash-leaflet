104205aa75bc5e0a28ec1dd3af80fda847fc5f32
Jannis Kupka <jannis.kupka@uni-kassel.de>
2020-04-22 11:09:56 +0200

    GeoJSON with json string

--- src/lib/components/GeoJSON.react.js
+++ src/lib/components/GeoJSON.react.js
@@ -2,20 +2,28 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import { GeoJSON as GeoJSONLeaflet } from 'react-leaflet';
+import { Circle } from 'react-leaflet';
 
 export default class GeoJSON extends Component {
     render() {
         // We need to use the non-JSX syntax to avoid having to list all props
-        const el = React.createElement(
-            GeoJSONLeaflet,
-            this.props,
-            this.props.children
-        )
+	let circles = []
+	let data = JSON.parse(this.props.data)
+	for(const obj of data){
+		if (obj.properties.hasOwnProperty("radius")){
+			circles.push(
+				React.createElement(
+					Circle,
+					{center: obj.geometry.coordinates, radius: obj.properties.radius}
+				)
+			)
+		}
+	}
 
-        return el
+        return circles 
     }
 }
 
 GeoJSON.propTypes = {
-	data: PropTypes.object,
+	data: PropTypes.string
 }
--- usage.py
+++ usage.py
@@ -1,23 +1,40 @@
 import dash
+
 import dash_leaflet as dl
 import dash_html_components as html
+import numpy as np
+import random
+import json
+
+center = np.array([56.05, 10.25])
+randx = [random.random() for i in range(1000)]
+randy = [random.random() for i in range(1000)]
+
+points = np.array(list(zip(randx, randy)))
+
+circles = []
+for idx, point in enumerate(points):
+    circle = {
+        "key": str(idx),
+        "properties": {
+            "radius": 10
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": (center + point).tolist()
+        }
+    }
+    circles.append(circle)
+
+circles = json.dumps(circles)
+
 
 app = dash.Dash()
 app.layout = html.Div([
-    dl.Map(style={'width': '1000px', 'height': '500px'}, center=[0, 0], zoom=10, children=[
+    dl.Map(style={'width': '1000px', 'height': '500px'}, center=center, zoom=10, children=[
         dl.TileLayer(),
-        dl.GeoJSON(data={
-            "type": "Feature",
-            "properties": {
-                    "name": "Coors Field",
-                    "amenity": "Baseball Stadium",
-                    "popupContent": "This is where the Rockies play!",
-                },
-            "geometry": {
-                    "type": "Point",
-                    "coordinates": [0, 0]
-                }
-        })
+        dl.Circle(center=center.tolist(), radius=10)
+#         dl.GeoJSON(data=Circle([1, 0]))
     ])
 ])
 
